bd91d7e7244e86e9fa72823fd9b3e44fc25e49b4
Aarón Freire <freiresousa97@gmail.com>
2024-06-07 10:30:25 +0200

    spiders actualizadas

---
+++ web_crawler/web_crawler/spiders/vinted.py
@@ -0,0 +1,94 @@
+import scrapy
+from scrapy_selenium import SeleniumRequest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import json
+class VintedSpider(scrapy.Spider):
+    name = "vinted"
+
+    def start_requests(self):
+        self.item_data = []
+        url = "https://www.vinted.es/catalog?search_text=watch&order=newest_first&brand_ids[]=194842&brand_ids[]=7467552"  # URL base
+        yield SeleniumRequest(url=url, callback=self.parse)
+
+    def parse(self, response):
+    # Obtén una instancia del controlador de Selenium
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        driver = webdriver.Chrome(options=options)
+
+        # Navega a la URL
+        driver.get(response.url)
+
+        # Espera a que aparezcan los elementos con la clase especificada
+        wait = WebDriverWait(driver, 15)
+        feed_items = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.feed-grid__item-content')))
+
+        parsed_items = 0
+
+        # Itera sobre los elementos y extrae los datos
+        for item in feed_items:
+            url = item.find_element(By.CSS_SELECTOR, 'a.new-item-box__overlay--clickable').get_attribute('href')
+            yield SeleniumRequest(url=url, callback=self.parse_item)
+            parsed_items += 1
+            if parsed_items >= 2:
+                break
+        # Cierra el controlador de Selenium
+        driver.quit()
+
+    def parse_item(self, response):
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        driver = webdriver.Chrome(options=options)
+
+        driver.get(response.url)
+
+        brand_element = driver.find_element(By.CSS_SELECTOR, 'a.inverse.u-disable-underline-without-hover')
+
+        brand = brand_element.find_element(By.TAG_NAME, 'span').text
+
+        location_element = driver.find_element(By.CSS_SELECTOR, 'div[data-testid="item-details-location"] div.details-list__item-value')
+
+        location = location_element.text
+        view_count_element = driver.find_element(By.CSS_SELECTOR, 'div[data-testid="item-details-view_count"] div.details-list__item-value')
+        
+        
+        price_container_element = driver.find_element(By.CSS_SELECTOR, 'div[data-testid="item-sidebar-price-container"]')
+        price_element = price_container_element.find_element(By.CSS_SELECTOR, 'div.u-flexbox.u-align-items-center[data-testid="item-price"]')
+
+        price_text_element = price_element.find_element(By.TAG_NAME, 'h3')
+        price = price_text_element.text
+        price = price.replace('€', '').replace(',', '')
+
+          
+        
+        
+        image_div = driver.find_element(By.CSS_SELECTOR, 'div[data-testid="item-photo-1"]')
+        image_src = image_div.find_element(By.TAG_NAME, 'img').get_attribute('src')
+
+        views = view_count_element.text
+
+
+
+
+        # Agregar el objeto a self.item_data
+        self.item_data.append({
+            
+            'image_src' : image_src,
+            'price': price,
+            'brand': brand,
+            'location': location,
+            'views' : views,
+        })
+
+        # Agregar el objeto a self.item_data
+
+        # Cierra el controlador de Selenium
+        driver.quit()
+    def close(self, reason):
+        if reason == 'finished':
+            with open('vinted.json', 'w', encoding='utf-8') as f:
+                json.dump(self.item_data, f, indent=5, ensure_ascii=False)
--- web_crawler/web_crawler/spiders/wallapop.py
+++ web_crawler/web_crawler/spiders/wallapop.py
@@ -12,6 +12,7 @@ class WallapopSpider(scrapy.Spider):
     name = "wallapop"
 
     def start_requests(self):
+        self.item_data = []
         url = "https://es.wallapop.com/app/search?filters_source=search_box&keywords=omega%20watch&longitude=-3.69196&latitude=40.41956"
         yield SeleniumRequest(url=url, callback=self.parse)
 
@@ -32,15 +33,70 @@ class WallapopSpider(scrapy.Spider):
         # Obtén los elementos con la clase especificada
         items = driver.find_elements(By.CSS_SELECTOR, '.ItemCardList__item')
 
+        parsed_items = 0
+
         # Itera sobre los elementos y extrae los datos
-        data = []
         for item in items:
             item_text = item.get_attribute('title')
             item_url = item.get_attribute('href')
-            data.append({
-                'item_text': item_text,
-                'item_url' : item_url,
-            })
+        
+            yield SeleniumRequest(url=item_url, callback=self.parse_item, meta={'item_text': item_text})
+            parsed_items += 1
+            if parsed_items >= 2:
+                break
 
-        with open('datos.json', 'w') as f:
-            json.dump(data, f)
+   
+            
+
+    def parse_item(self, response):
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        driver = webdriver.Chrome(options=options)
+
+        driver.get(response.url)
+
+        wait = WebDriverWait(driver, 15)
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.pb-5')))
+
+        # Obtener el elemento .pb-5
+        pb5_element = driver.find_element(By.CSS_SELECTOR, '.pb-5')
+
+        # Obtener las imágenes
+        image = pb5_element.find_element(By.TAG_NAME, 'img')
+        # valoracion = pb5_element.find_element(By.CSS_SELECTOR, 'item-detail-header_ItemDetailHeader__text--typoLowS__9JNQi').text
+
+        # Obtener el precio
+        price = pb5_element.find_element(By.CSS_SELECTOR, '.item-detail-price_ItemDetailPrice--standard__TxPXr').text
+        price = price.replace('€', '')
+        
+        title = pb5_element.find_element(By.CSS_SELECTOR, 'h1.item-detail_ItemDetail__title__wcPRl').text
+
+
+        description = pb5_element.find_element(By.CSS_SELECTOR, 'section.item-detail_ItemDetail__description__7rXXT').text
+
+
+
+
+        item_url = response.url
+
+        image_src = image.get_attribute('src')
+        item_data = {
+        'title': title,
+        'image_src': image_src,
+        'price': price,
+        # 'valoracion': valoracion,
+        'description': description,
+        'url': item_url
+
+        }
+
+
+        self.item_data.append(item_data)
+        
+        driver.quit()
+
+
+    def close(self, reason):
+        if reason == 'finished':
+            with open('wallapop.json', 'w') as f:
+                json.dump(self.item_data, f, indent=4, ensure_ascii=False)        
